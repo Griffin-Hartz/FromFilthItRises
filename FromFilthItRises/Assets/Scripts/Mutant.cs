718d26155d571cca6c97cb72be25ef3c02e45cc4
Griffin-Hartz <hartzg@wwu.edu>
2023-04-22 21:18:28 -0700

    animates with mlagents!
    Had to set update mode on the animator to physics based

--- FromFilthItRises/Assets/Scripts/Mutant.cs
+++ FromFilthItRises/Assets/Scripts/Mutant.cs
@@ -7,6 +7,9 @@ using System;
 public class Mutant : EnemyAgent
 {
     Animator animator;
+    float localSpeed = 0;
+    float localTurn = 0;
+    bool attacking = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +19,31 @@ public class Mutant : EnemyAgent
     public override void OnActionReceived(ActionBuffers actions)
     {
         //Vector3 move = new Vector3(actions.ContinuousActions[0], actions.ContinuousActions[1], actions.ContinuousActions[2]);
-        animator.SetFloat("Speed", actions.ContinuousActions[0]);
-        animator.SetFloat("Turn", actions.ContinuousActions[1]);
+        localSpeed = actions.ContinuousActions[0];
+        localTurn = actions.ContinuousActions[1];
+        //animator.
         if (actions.ContinuousActions[2] > 0.99)
+        {
+            attacking = true;
+        }
+        else
+        {
+            attacking = false;
+        }
+        Debug.Log("Test Speed: " + Math.Round(actions.ContinuousActions[0], 3));
+    }
+
+    private void FixedUpdate()
+    {
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", localSpeed);
+            animator.SetFloat("Turn", localTurn);
+        }
+        if (attacking)
         {
             animator.SetTrigger("Attack");
         }
-        Debug.Log("Test Speed: " + Math.Round(actions.ContinuousActions[0], 3));
     }
 
     //should add an onepisodebegin clause so that it doesn't spawn on top of player maybe

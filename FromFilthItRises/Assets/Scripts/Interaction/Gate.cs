294aeb95181dc83a65734b6d87d274d659477c44
Griffin-Hartz <hartzg@wwu.edu>
2023-04-30 21:10:43 -0700

    gate works

--- FromFilthItRises/Assets/Scripts/Interaction/Gate.cs
+++ FromFilthItRises/Assets/Scripts/Interaction/Gate.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Gate : Interactable
 {
     [SerializeField] private GameObject gate;
+    [SerializeField] private TextMeshProUGUI tm;
     public override void Interact()
     {
-        Debug.Log("Interact");
+        try
+        {
+            tm.gameObject.SetActive(false);
+        }
+        catch { }
         gate.SetActive(false);
     }
 
     public override void OnTriggerStay(Collider other)
     {
         base.OnTriggerStay(other);
-        if(other.gameObject.tag == "Player")
-        {
-            Debug.Log("Trigger");
-        }
     }
 }

ac2b34c3e5079e59ae2b3068df2017cd6634239c
Griffin Hartz <59674096+Griffin-Hartz@users.noreply.github.com>
2023-05-02 19:27:03 -0700

    fix?

--- FromFilthItRises/Assets/Scripts/ML/Mutant.cs
+++ FromFilthItRises/Assets/Scripts/ML/Mutant.cs
@@ -7,6 +7,9 @@ using System;
 public class Mutant : EnemyAgent
 {
     Animator animator;
+    float localSpeed = 0;
+    float localTurn = 0;
+    bool attacking = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +18,45 @@ public class Mutant : EnemyAgent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        //Vector3 move = new Vector3(actions.ContinuousActions[0], actions.ContinuousActions[1], actions.ContinuousActions[2]);
-        animator.SetFloat("Speed", actions.ContinuousActions[0]);
-        animator.SetFloat("Turn", actions.ContinuousActions[1]);
-        if (actions.ContinuousActions[2] > 0.99)
+        localSpeed = actions.ContinuousActions[0];
+        localTurn = actions.ContinuousActions[1];
+        //My plan is to have a separate, non-ml script on the mutant that will handle things like sounds, the attack animation, etc 
+        //that would be really hard to train the agent to do
+    }
+
+    //I wish I could make it reset on player kill
+
+    private void FixedUpdate()
+    {
+        if (animator != null)
         {
+            animator.SetFloat("Speed", localSpeed);
+            animator.SetFloat("Turn", localTurn);
+        }
+    }
+
+    public override void OnCollisionEnter(Collision collision)
+    {
+        if (trainingMode && collision.collider.CompareTag("Boundary"))// 
+        {
+            // Collided with the area boundary, give a negative reward
+            Debug.Log("Collide with boundary");
+            AddReward(-.2f);
+        }
+        if (trainingMode && collision.collider.CompareTag("Building"))// 
+        {
+            // Collided with the area boundary, give a negative reward
+            Debug.Log("Collide with building");
+            AddReward(-.1f);
+        }
+        if (trainingMode && collision.collider.CompareTag("Player") && nearestPlayer.isAlive)// && validRun
+        {
+            nearestPlayer.Kill();
+            AddReward(10);
+            Debug.Log("Kill player");
+            OnEpisodeBegin();
             animator.SetTrigger("Attack");
         }
-        Debug.Log("Test Speed: " + Math.Round(actions.ContinuousActions[0], 3));
     }
 
     //should add an onepisodebegin clause so that it doesn't spawn on top of player maybe
